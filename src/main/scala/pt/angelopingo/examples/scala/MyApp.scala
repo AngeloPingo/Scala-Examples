a801c4ad56364a611239349c1ab7c3c980889374
Angelo Pingo <angelo.pingo@celfocus.com>
2019-02-12 16:21:02 +0000

    Add:   5 - Maps example   6 - Tuples example   7 - Arrays example

--- src/main/scala/pt/angelopingo/examples/scala/MyApp.scala
+++ src/main/scala/pt/angelopingo/examples/scala/MyApp.scala
@@ -1,27 +1,30 @@
 package pt.angelopingo.examples.scala
 
-import pt.angelopingo.examples.scala.basics.{ErrorHandling, ReadFromConsole, ReadFromFile, Sets}
+import pt.angelopingo.examples.scala.basics._
 
 object MyApp {
 
   def main(args: Array[String]): Unit = {
-    val option = printAndSetOption
-    chooseAndRunExample(option)
+    val exampleSelected = printAndSetExample
+    runSelectedExample(exampleSelected)
   }
 
-  private def printAndSetOption = {
+  private def printAndSetExample = {
     print(
-      "Want do you want?\n" +
+      "What do you want?\n" +
         "1 - Read from console example.\n" +
         "2 - Read from file example.\n" +
         "3 - Error handling example.\n" +
-        "4 - Sets example.\n"
+        "4 - Sets example.\n" +
+        "5 - Maps example.\n" +
+        "6 - Tuples example.\n" +
+        "7 - Arrays example.\n"
     )
     val option = readInt()
     option
   }
 
-  private def chooseAndRunExample(option: Int): Unit = {
+  private def runSelectedExample(option: Int): Unit = {
     option match {
       case 1 =>
         val readFromConsole = new ReadFromConsole()
@@ -36,6 +39,15 @@ object MyApp {
       case 4 =>
         val sets = new Sets()
         sets.example()
+      case 5 =>
+        val maps = new Maps()
+        maps.example()
+      case 6 =>
+        val tuples = new Tuples()
+        tuples.examples()
+      case 7 =>
+        val arrays = new Arrays()
+        arrays.examples()
       case whoa => println("Invalid option: " + whoa.toString)
     }
   }
--- src/main/scala/pt/angelopingo/examples/scala/basics/Sets.scala
+++ src/main/scala/pt/angelopingo/examples/scala/basics/Sets.scala
@@ -3,20 +3,23 @@ package pt.angelopingo.examples.scala.basics
 class Sets {
 
   def example(): Unit = {
-    var nums1 = Set(1, 2, 3, 4, 5)
-    var nums2 = Set(10, 20, 30, 40, 50)
+    var numbers1 = Set(1, 2, 3, 4, 5)
+    val numbers2 = Set(10, 20, 30, 40, 50)
 
-    println("[Contains element]\t(nums1.contains(4))\t: " + nums1.contains(4))
-    println("[Contains element]\t(nums1(2))\t: " + nums1(2)) // same of contains
-    println("[Mix 2 Sets]\t\t(nums1 ++ nums2)\t: " + (nums1 ++ nums2)) // no guarantees order
-    nums1 -= 1
-    println("[Remove Element]\t(nums1 -= 1)\t: " + nums1)
-    nums1 += 10
-    println("[Add Element]\t\t(nums1 += 10)\t: " + nums1)
-    println("[Intersection]\t\t(nums1 & nums2)\t: " + (nums1 & nums2))
-    println("[First Element]\t\t(nums1.head)\t: " + nums1.head)
-    println("[All Element except first]\t\t(nums1.tail)\t: " + nums1.tail)
-    println("[Is empty?]\t\t(nums1.isEmpty)\t: " + nums1.isEmpty)
+    println("[INITIAL STATE] numbers1: " + numbers1)
+    println("[INITIAL STATE] numbers2: " + numbers2)
+    println()
+    println("[Contains element?]\t(numbers1.contains(4)):\t" + numbers1.contains(4))
+    println("[Contains element?]\t(numbers1(2)):\t" + numbers1(2)) // same of contains
+    println("[Mix 2 Sets]\t\t(numbers1 ++ numbers2):\t" + (numbers1 ++ numbers2)) // no guarantees order
+    numbers1 -= 1
+    println("[Remove element]\t(numbers1 -= 1):\t" + numbers1)
+    numbers1 += 10
+    println("[Add element]\t\t(numbers1 += 10):\t" + numbers1)
+    println("[Intersection]\t\t(numbers1 & numbers2):\t" + (numbers1 & numbers2))
+    println("[First element]\t\t(numbers1.head):\t" + numbers1.head)
+    println("[All element except first]\t\t(numbers1.tail):\t" + numbers1.tail)
+    println("[Is empty?]\t\t(numbers1.isEmpty):\t" + numbers1.isEmpty)
   }
 
 }
